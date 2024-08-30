aea40f31a126dd82a5342dcb3b51ac640c1c8387
Sanjay Seshan <seshan@mit.edu>
2024-08-29 22:14:20 -0400

    added français

--- src/assets/missions.js
+++ src/assets/missions.js
@@ -11,78 +11,7 @@ function specialCasesCheck(mission) {
     if (specialCasesComplete != 1) {
         specialCasesComplete = 1
     } else { return }
-    // console.log(document.getElementById("yestransportationJourney1").checked)
-
-    // if (mission == "transportationJourney1") {
-    //     if (document.getElementById("yestransportationJourney1").checked && document.getElementById("yestransportationJourney2").checked) {
-    //         recalc(20, "transportationJourney2", 1)
-    //         recalc(10, "transportationJourney1", 1)
-    //     } else {
-    //         recalc(10 * document.getElementById("yestransportationJourney2").checked, "transportationJourney1", document.getElementById("yestransportationJourney2").checked*1)
-    //     }
-    // }
-    // if (mission == "transportationJourney2") {
-    //     if (document.getElementById("yestransportationJourney1").checked && document.getElementById("yestransportationJourney2").checked) {
-    //         recalc(20, "transportationJourney1", 1)
-    //         recalc(10, "transportationJourney2", 1)
-    //     } else {
-    //         recalc(10 * document.getElementById("yestransportationJourney1").checked, "transportationJourney1", document.getElementById("yestransportationJourney1").checked*1)
-    //     }
-    // }
-
-    // if (mission == "platooningTrucks1") {
-    //     if (document.getElementById("yesplatooningTrucks1").checked && document.getElementById("yesplatooningTrucks2").checked) {
-    //         recalc(20, "platooningTrucks2", 1)
-    //         recalc(10, "platooningTrucks1", 1)
-    //     } else {
-    //         recalc(10 * document.getElementById("yesplatooningTrucks2").checked, "platooningTrucks1", document.getElementById("yesplatooningTrucks2").checked*1)
-    //     }
-    // }
-    // if (mission == "platooningTrucks2") {
-    //     if (document.getElementById("yesplatooningTrucks1").checked && document.getElementById("yesplatooningTrucks2").checked) {
-    //         recalc(20, "platooningTrucks1", 1)
-    //         recalc(10, "platooningTrucks2", 1)
-    //     } else {
-    //         recalc(10 * document.getElementById("yesplatooningTrucks1").checked, "platooningTrucks1", document.getElementById("yesplatooningTrucks1").checked*1)
-    //     }
-    // }
-
-    // if (mission == "loadCargo1" || mission == "loadCargo2" || mission == 'loadCargo3' || mission == 'cargoConnect1' || mission == 'cargoConnect2') {
-    //     if (document.getElementById("yessortingCenter1").checked) {
-    //         totalContainers = 7
-    //     } else {
-    //         totalContainers = 8
-    //     }
-    //     console.log(totalContainers);
-    //     sum = parseInt(document.getElementById("loadCargo1").value) + parseInt(document.getElementById("loadCargo2").value) + parseInt(document.getElementById("loadCargo3").value) +  parseInt(document.getElementById("cargoConnect1").value) + parseInt(document.getElementById("cargoConnect2").value)
-    //     if (sum > totalContainers) {
-    //         document.getElementById(mission).value = totalContainers - (sum - document.getElementById(mission).value)
-    //         sum = totalContainers
-    //         recalc(window[mission + 'inc'] * parseInt(document.getElementById(mission).value), mission, parseInt(document.getElementById(mission).value))
-    //     }
-    //     if (sum == 8) {
-    //         $('#nosortingCenter1').checkboxradio('disable');
-    //         $('#yessortingCenter1').checkboxradio('disable');
-    //         document.getElementById('yessortingCenter1').checked = false
-    //         document.getElementById('nosortingCenter1').checked = true
-    //     } else {
-    //         $('#nosortingCenter1').checkboxradio('enable');
-    //         $('#yessortingCenter1').checkboxradio('enable');
-    //     }
-    // }
-    // if (mission == "cargoConnect2") {
-    //     if (parseInt(document.getElementById("cargoConnect2").value) == 1) {
-    //         createbuttonconflict("cargoConnect3", "cargoConnect4", 0)
-    //     } else if (parseInt(document.getElementById("cargoConnect2").value) > 1) {
-    //         deletebuttonconflict("cargoConnect3", "cargoConnect4", 0)
-    //     }
-    // }
-
-    // if ((mission == "cargoConnect5" || mission == "cargoConnect2") && parseInt(document.getElementById("cargoConnect5").value) > parseInt(document.getElementById("cargoConnect2").value)) {
-    //     document.getElementById("cargoConnect5").value = parseInt(document.getElementById("cargoConnect2").value);
-    //     recalc(window["cargoConnect5" + 'inc'] * parseInt(document.getElementById("cargoConnect5").value), "cargoConnect5", parseInt(document.getElementById("cargoConnect5").value))
-    // }
-
+    
     specialCasesComplete = 0
 
 }
@@ -115,9 +44,7 @@ function specialCasesRecalc(mission) {
 
 function load2021() {
     blanksave = "m00a|0,m01a|0,m01b|0,m01c|0,m02a|0,m02b|0,m03a|0,m03b|0,m04a|0,m04b|0,m05a|0,m06a|0,m07a|0,m08a|0,m09a|0,m09b|0,m10a|0,m10b|0,m11a|0,m11b|0,m12a|0,m13a|0,m14a|0,m14b|0,m14c|0,m14d|0,m14e|0,m15a|0,m15b|0,m15c|0,m15d|0,precision1|0,graciousProfessionalism|0"
-    // blanksave = "advantage1|0,innovationProject1|0,unusedCapacity1|0,unloadCargoPlane1|0,unloadCargoPlane2|0,transportationJourney1|0,transportationJourney2|0,switchEngine1|0,accidentAvoidance1|0,accidentAvoidance2|0,unloadCargoShip1|0,unloadCargoShip2|0,airDrop1|0,airDrop2|0,airDrop3|0,trainTracks1|0,trainTracks2|0,sortingCenter1|0,homeDelivery1|0,homeDelivery2|0,largeDelivery1|0,largeDelivery2|0,largeDelivery3|0,platooningTrucks1|0,platooningTrucks2|0,bridge1|0,loadCargo1|0,loadCargo2|0,loadCargo3|0,cargoConnect1|0,cargoConnect2|0,cargoConnect3|0,cargoConnect4|0,cargoConnect5|0,precision1|0,graciousProfessionalism|0"
-    // Override function in scoring system during the checking for button conflicts
-
+    
 
     clearbuffer()
 
@@ -125,47 +52,33 @@ function load2021() {
 
 
     starttable('A00', "EQUIPMENT INSPECTION", 'M00.png', ['m00a'], 0)
-    createbutton("m00a", 20, "If your robot and all your equipment fi t completely in one launch area and are under a height limit of 12 in. (305 mm) during the pre-match inspection:")
+    createbutton("m00a", 20, "If your robot and all your equipment fi t completely in one launch area and are under a height limit of 12 in. (305 mm) during the pre-match inspection")
     endtable()
 
     starttable('M01', "CORAL NURSERY", 'M01.png', ['m01a','m01b','m01c'], 1)
     createbutton("m01a", 20, "If the coral tree is hanging on on the coral tree support")
     createbutton("m01b", 10, "Bonus: and the bottom of the coral tree is in its holder")
     createbutton("m01c", 20, "If the coral buds are flipped up")
-    createcomment("No equipment may be touching any part of this mission’s mission model at the end of the match, to score for this mission.")
+    createcomment("No equipment may be touching any part of this mission's mission model at the end of the match, to score for this mission.")
     endtable()
 
     starttable('M02', "SHARK", 'M02.png', ['m02a','m02b'], 0)
     createbutton("m02a", 20, "If the shark is no longer touching the cave")
     createbutton("m02b", 10, "If the shark is touching the mat and it is at least partly in the shark habitat")
-    // createdropdown("m02a", ["","Blue", "Pink","Orange"], [0, 10, 20, 30], "If your theater's red fl ag is down and the active scene color is:")
-    // createdropdown("m02b", ["","Blue", "Pink", "Orange"], [0, 20, 30, 10], "BONUS: And if both teams' active scenes match:")
-    // createdropdown("stepCounter1", ["","Partly full of contents", "Completely full of contents"], [0, 20, 30, 20], "The hinged container is completely closed")
-    // createbutton("unusedCapacity1", 20, "Is the hinged container completely closed and partly full of contents (1-5 content pieces)?")
-    // createbutton("unusedCapacity2", 30, "Is the hinged container completely closed and completely full of contents (All 6 content pieces)?")
-    // create3button('unusedCapacity1',20,30,"Is the hinged container completely closed and partly full of contents (1-5 content pieces) or completely full of contents (All 6 content pieces)?")
-    // createbutton("stepCounter3", 20 , "M02c")
-    // createbuttonconflict("unusedCapacity1", "unusedCapacity2", 0)
-    // createbuttonconflict("stepCounter2","stepCounter3",0)
-    // createbuttonconflict("stepCounter1","stepCounter3",0)
+   
     endtable()
 
     starttable('M03', "CORAL REEF", 'M03.png', ['m03a','m03b'], 1) //1 = Note Present
     createbutton("m03a", 20, "If the coral reef is flipped up, not touching the mat")
     createrange("m03b", 5, 0, 3, 0, "If a reef segment is standing upright, outside of home, and touching the mat", '')
-    createcomment("No equipment may be touching any part of this mission’s mission model at the end of the match, to score for this mission.")
+    createcomment("No equipment may be touching any part of this mission's mission model at the end of the match, to score for this mission.")
 
     endtable()
-
-    // createbuttonconflict("slide1","slide2",0)
-    // createbuttonenables("slide1", "slide3")
-    // createbuttonenables("slide1", "slide4")
-    // endtable()
 
     starttable('M04', "SCUBA DIVER", 'M04.png', ['m04a', 'm04b'], 1)
     createbutton("m04a", 20, "If the scuba diver is no longer touching the coral nursery")
     createbutton("m04b", 20, "If the scuba diver is hanging on the coral reef support")
-    createcomment("The “coral nursery” includes any part of the Mission 01 mission model.")
+    createcomment("The 'coral nursery' includes any part of the Mission 01 mission model.")
     
     endtable()
 
@@ -176,20 +89,20 @@ function load2021() {
     starttable('M06', "RAISE THE MAST", 'M06.png', ['m06a'], 2)
     createbutton("m06a", 30, "If the shipwreck's mast is completely raised")
     createcomment("The shipwreck's mast is considered raised when the latch prevents it from returning to its starting position.")
-    createcomment("No equipment may be touching any part of this mission’s mission model at the end of the match, to score for this mission.")
+    createcomment("No equipment may be touching any part of this mission's mission model at the end of the match, to score for this mission.")
 
     endtable()
 
     starttable('M07', "KRAKEN'S TREASURE", 'M07.png', ['m07a'], 1)
     createbutton("m07a", 20, "If the treasure chest is completely outside the kraken's nest")
-    createcomment("No equipment may be touching any part of this mission’s mission model at the end of the match, to score for this mission.")
+    createcomment("No equipment may be touching any part of this mission's mission model at the end of the match, to score for this mission.")
 
     endtable()
 
     starttable('M08', "ARTIFICIAL HABITAT", 'M08.png', ['m08a'], 2)
     createrange("m08a", 10, 0, 4, 0, "If an artificial habitat stack segment is completely flat and upright", '')
     createcomment("There are four segments of the artificial habitat stack, each defined by its yellow base. A segment is considered upright when the crab is above its yellow base.")
-    createcomment("No equipment may be touching any part of this mission’s mission model at the end of the match, to score for this mission.")
+    createcomment("No equipment may be touching any part of this mission's mission model at the end of the match, to score for this mission.")
 
     endtable()
 
@@ -202,7 +115,7 @@ function load2021() {
     createbutton("m10a", 30, "If your team's yellow flag is down")
     createbutton("m10b", 10, "If the submersible is clearly closer to the opposing field:")
     createcomment("Teams may not block the opposing team. It is not possible to earn the bonus in remote competitions or if there is no opposing team.")
-    createcomment("No equipment may be touching any part of this mission’s mission model at the end of the match, to score for this mission.")
+    createcomment("No equipment may be touching any part of this mission's mission model at the end of the match, to score for this mission.")
 
     endtable()
 
@@ -213,9 +126,8 @@ function load2021() {
 
     starttable('M12', "FEED THE WHALE", 'M12.png', ['m12a'], 1)
     createrange("m12a", 10, 0, 5, 0, "Krill at least partly in the whale's mouth", '')
-    createcomment("No equipment may be touching any part of this mission’s mission model at the end of the match, to score for this mission.")
+    createcomment("No equipment may be touching any part of this mission's mission model at the end of the match, to score for this mission.")
 
-    // createbutton("m12a", 10, "Krill at least partly in the whale's mouth")
     endtable()
 
     starttable('M13', "CHANGING SHIPPING LANES", 'M13.png', ['m13a'], 0)
@@ -236,7 +148,7 @@ function load2021() {
     createrange("m15b", 5, 0, 2, 0, "Trident Part(s)", '')
     createbutton("m15c", 5, "Treasure Chest")
     createbutton("m15d", 10, "If the port's latch is at least partly in the research vessel's loop")
-    createcomment("No equipment may be touching any part of this mission’s mission model at the end of the match, to score for this mission.")
+    createcomment("No equipment may be touching any part of this mission's mission model at the end of the match, to score for this mission.")
 
     endtable()
 
